8ac0e207f23b759bf038a7956e68b3a09efd3cf4
Khushkaran Singh Bajwa <khushkaran@users.noreply.github.com>
2014-10-28 11:08:50 +0000

    feat(trigger): trigger getpet action with the Pet Name
    When clicking one of the pet names it sends a trigger with the Pet Name.
    This will work in conjunction with Pet Finder app, to allow an update of
    the DOM with the new Pet Name.

--- js/main.js
+++ js/main.js
@@ -2,4 +2,13 @@ $(document).ready(function() {
   Livestax.on("pet-finder.newpet", function(petName) {
     $(".js-pet-names").prepend("<a href='#' class='list-group-item'>" + petName + "</a>");
   });
+
+  $(document.body).on("click", ".js-pet-names a", function(event) {
+    var clickedLink, petName;
+    clickedLink = event.target;
+    petName = clickedLink.text;
+    $(".js-pet-names a").removeClass("active");
+    $(clickedLink).addClass("active");
+    Livestax.store.set("getpet", petName);
+  });
 });
