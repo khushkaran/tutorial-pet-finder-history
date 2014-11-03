8dd19f94aa9fd5e144fc8e62a0dd4b5831265acc
Khushkaran Singh Bajwa <khushkaran@users.noreply.github.com>
2014-11-03 14:12:37 +0000

    feat(signed_request): verify and decode
    Using the JWT recommended package for verifying a JSON Web Token, the
    signed request is verified. However, to verify we require the app
    secret that should not be stored in the code, as it is a configurable
    variable, therefore we are storing it within a `.env.json` file and it
    is imported using the "dot-env" NPM package.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-var express, app, bodyParser;
+require('dot-env');
+var express, app, bodyParser, jwt, appSecret;
 express = require('express');
 app = express();
 bodyParser  = require('body-parser');
+jwt = require('jsonwebtoken');
+appSecret = process.env.APP_SECRET;
 
 app.set('port', (process.env.PORT || 5000));
 app.set('view engine', 'ejs');
@@ -12,6 +15,8 @@ app.use(bodyParser.urlencoded( {
 
 app.all('/', function(request, response) {
   var signedRequest = request.body.signed_request;
+  jwt.verify(signedRequest, appSecret, function(err, decoded) {
+  });
   response.render('index');
 });
 
