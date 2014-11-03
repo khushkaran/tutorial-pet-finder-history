8eaa34ffb00eb3621e10fb511ac97d7fbe57a850
Khushkaran Singh Bajwa <khushkaran@users.noreply.github.com>
2014-11-03 13:32:12 +0000

    feat(signed_request): capture and assign to variable
    LiveStax provides a Signed Request as a JSON Web Token (http://jwt.io),
    however, to get the token, in Node.js, the parameter body needs to be
    parsed with a package called `body-parser`. The package has been added
    to `package.json` for easy install using the `npm install` command. The
    JSON Web Token will still need to be decoded.
    
    To use the signed request that LiveStax provides, in Node.js this needs
    to be parsed with a package called `body-parser`, which has been added
    to `package.json`. This token, however, will still need to be decoded.

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-var express, app;
+var express, app, bodyParser;
 express = require('express');
 app = express();
+bodyParser  = require('body-parser');
 
 app.set('port', (process.env.PORT || 5000));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded( {
+  extended: true
+}));
 
 app.all('/', function(request, response) {
+  var signedRequest = request.body.signed_request;
   response.render('index');
 });
 
